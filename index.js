1c2674302c79d42b1a966bbf448c0838a833c9ae
Puneet Dwivedi <pdwivedi294@gmail.com>
2022-11-06 19:09:23 +0530

    vercel.json fixed

--- index.js
+++ index.js
@@ -7,48 +7,48 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "/atwoj_clone/build")));
+app.use(express.static(path.join(__dirname, "atwoj_clone", "build")));
 
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname,"atwoj_clone","build","index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "atwoj_clone", "build", "index.html"));
 })
 
-app.get("/api/ladder_problem", (req, res)=>{
-    try{
+app.get("/api/ladder_problem", (req, res) => {
+    try {
         const query = req.query;
 
         // path of ladder
         let ladderPath;
 
-        if(query.division !== undefined && query.problem_level !== undefined){
+        if (query.division !== undefined && query.problem_level !== undefined) {
             ladderPath = path.join(__dirname, "ladder_problem", `div${query.division}${query.problem_level}`);
         }
-        else if(query.problem_rating !== undefined){
+        else if (query.problem_rating !== undefined) {
             ladderPath = path.join(__dirname, "ladder_problem", `rating${query.problem_rating}`);
         }
-        else{
+        else {
             throw "Requested ladder is not found";
         }
-        
+
         // requiring ladder
         const ladder = require(ladderPath);
-        
+
         // sending response
-        res.status(200).json({success : "ok", response : ladder});
+        res.status(200).json({ success: "ok", response: ladder });
     }
-    catch(error){
-        res.status(500).json({success : "fail", error : "Requested ladder is not found"});
+    catch (error) {
+        res.status(500).json({ success: "fail", error: "Requested ladder is not found" });
     }
 });
 
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/atwoj_clone/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/atwoj_clone/build', 'index.html'));
 });
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("started")
 });
